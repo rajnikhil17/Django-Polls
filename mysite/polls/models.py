50adb25b83712fa31ce1ea21e26b6a38444847db
rajnikhil17 <rajmarpu575@gmail.com>
2018-09-18 13:10:17 +0530

    Difference Between Model and Serializer

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,7 +5,13 @@ from django.db import models
 
 # Create your models here.
 
+#Model fields are what you keep in your database.
+#These are visible to only the developer
+#(it answers how you want your data organized)
 
+#Serializer fields are what you expose to your clients.
+#(it answers how you want your data represented)
+#while Serializer fields are used to when exposing the api to the client, visible to client also.
 
 class Question(models.Model):
 	question_text= models.CharField(max_length=200)
